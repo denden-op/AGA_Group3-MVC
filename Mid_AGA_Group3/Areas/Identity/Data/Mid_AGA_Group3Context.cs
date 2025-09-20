6d5726c51ff120e2ae31550a56c7e38a7edbd50d
Group3_AGA <souno@DESKTOP-JCMA66N>
2025-09-20 11:59:35 +0700

    test2

---
+++ Mid_AGA_Group3/Areas/Identity/Data/Mid_AGA_Group3Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mid_AGA_Group3.Data
+{
+    public class Mid_AGA_Group3Context : IdentityDbContext<IdentityUser>
+    {
+        public Mid_AGA_Group3Context(DbContextOptions<Mid_AGA_Group3Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
--- Mid_AGA_Group3/Program.cs
+++ Mid_AGA_Group3/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Mid_AGA_Group3.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Mid_AGA_Group3ContextConnection") ?? throw new InvalidOperationException("Connection string 'Mid_AGA_Group3ContextConnection' not found.");
+
+builder.Services.AddDbContext<Mid_AGA_Group3Context>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Mid_AGA_Group3Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
